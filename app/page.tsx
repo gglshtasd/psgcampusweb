fe7daec8018ffe0e9785ea1f9da5078cd8ccefab
FALULLAH S <gglshtasd@gmail.com>
2026-05-26 20:59:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>PSG Campus Web Wrapper</h1>
+      <p>System is online. Awaiting API testing...</p>
+    </main>
+  )
+}
